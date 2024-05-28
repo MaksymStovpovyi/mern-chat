86e7a695bf92c557380200e49ee9416872262078
max <stovpovymax@gmail.com>
2024-05-28 10:35:09 -0400

    models/user

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,12 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
-  res.send("root route");
-});
+dotenv.config();
 
 app.use("/api/auth",authRoutes);
-
+ 
 app.listen(PORT,() => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
