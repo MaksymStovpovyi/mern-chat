7b339fb9b65439ccfc16afb74b116a33c11db0b5
max <stovpovymax@gmail.com>
2024-05-29 11:05:39 -0400

    auth.contoller sigup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+app.use(express.json()); // parse from req.body
 
 app.use("/api/auth",authRoutes);
  
