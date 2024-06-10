3cf682eb5a0169763800667d23133445b7489b9a
max <stovpovymax@gmail.com>
2024-06-10 18:06:14 -0400

    routes/message

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -11,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // parse from req.body
 
 app.use("/api/auth", authRoutes);
- 
+app.use("/api/messages", messageRoutes);
+
+
 app.listen(PORT,() => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
