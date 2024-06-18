f91ceb4a17be666024cc56029ebbc479cd0371bd
max <stovpovymax@gmail.com>
2024-06-17 21:25:40 -0400

    middleware/protectRoute.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse from req.body
+app.use(cookieParser()); 
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
