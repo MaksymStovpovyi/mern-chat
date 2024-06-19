d04501a9520ec54b334dd61ae06fc8a69898956f
max <stovpovymax@gmail.com>
2024-06-18 21:30:46 -0400

    fixed bug in conversation.modal.js

--- backend/models/conversation.modal.js
+++ backend/models/conversation.modal.js
@@ -8,7 +8,7 @@ const conversationSchema = new mongoose.Schema (
         ref: "User"
       },
     ],
-    masseges: [
+    messages: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Message",
