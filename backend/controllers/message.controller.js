fc2d3b2af1a6c4e5656e1ed867d7113e2cfa297c
max <stovpovymax@gmail.com>
2024-06-20 21:12:37 -0400

    message controller is done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,6 +27,10 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    // todo socket here
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log('Error in sendMessage controller: ', error.message);
@@ -34,3 +38,26 @@ export const sendMessage = async (req, res) => {
   }
 };
  
+export const getMessages = async (req, res) => {
+
+  try {
+
+    const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages"); // not reference but actual messages
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+
+  } catch (error) {
+    console.log('Error in getMessages controller: ', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
+};
